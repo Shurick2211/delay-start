6d3059bd1231effb6742eb4f0361f1665add4526
Shurick2211 <Shurick2211@gmail.com>
2023-09-10 09:09:27 +0300

    settings

--- app/src/main/java/com/nimko/delayedstart/MainActivity.kt
+++ app/src/main/java/com/nimko/delayedstart/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.nimko.delayedstart
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.nimko.delayedstart.databinding.ActivityMainBinding
 import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var settings:SharedPreferences
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -16,21 +19,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        settings = getSharedPreferences("MySettings", MODE_PRIVATE)
         binding.apply {
             timePickerJob.apply {
                 setIs24HourView(true)
-                hour = 0
-                minute = 20
+                hour = settings.getInt(HOUR_JOB, 0)
+                minute = settings.getInt(MINUTE_JOB, 20)
                 setOnTimeChangedListener { timePicker, i, i2 -> run {
                     setResult(timePickerJob.hour, timePickerJob.minute, timePickerEnd.hour, timePickerEnd.minute)
                 }}
             }
             timePickerEnd.apply {
                 setIs24HourView(true)
-                hour = 6
-                minute = 30
+                hour = settings.getInt(HOUR_END, 6)
+                minute = settings.getInt(MINUTE_END, 30)
                 setOnTimeChangedListener { timePicker, i, i2 ->
                     run {
                         setResult(timePickerJob.hour, timePickerJob.minute, timePickerEnd.hour, timePickerEnd.minute)
@@ -39,7 +41,23 @@ class MainActivity : AppCompatActivity() {
             }
             setResult(timePickerJob.hour, timePickerJob.minute, timePickerEnd.hour, timePickerEnd.minute)
         }
+    }
 
+    @SuppressLint("CommitPrefEdits")
+    fun saveSettings() {
+        binding.apply {
+            settings.edit().apply{
+                putInt(HOUR_JOB, timePickerJob.hour)
+                putInt(MINUTE_JOB, timePickerJob.minute)
+                putInt(HOUR_END, timePickerEnd.hour)
+                putInt(MINUTE_END, timePickerEnd.minute)
+            }.apply()
+        }
+    }
+
+    override fun onPause() {
+        saveSettings()
+        super.onPause()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -64,14 +82,19 @@ class MainActivity : AppCompatActivity() {
                     "${nowTime.hour}:${nowTime.minute}"
             if (timeForSetHour > 23)
                 timeForSetHour = timeForSetHour - 24
-            //resources.getString(R.string.error)
             textResult.text =
                 if (timeForSetMinute in 0..9)
                     "$timeForSetHour:0$timeForSetMinute"
                 else
                     "$timeForSetHour:$timeForSetMinute"
         }
+    }
 
 
+    companion object{
+        const val HOUR_JOB = "defaultHourJob"
+        const val MINUTE_JOB = "defaultMinuteJob"
+        const val HOUR_END = "defaultHourEnd"
+        const val MINUTE_END = "defaultMinuteEnd"
     }
 }
